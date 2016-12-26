3e3f24f9ceb1e203dd0f7a69cc86470ef441ff98
xlu <xlu@qq.com>
2016-12-26 20:08:01 +0800

    添加素材

--- VirtualTrain/Properties/Resources.Designer.cs
+++ VirtualTrain/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace VirtualTrain.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap error {
+            get {
+                object obj = ResourceManager.GetObject("error", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap g01 {
             get {
                 object obj = ResourceManager.GetObject("g01", resourceCulture);
